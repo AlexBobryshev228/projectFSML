f676a911ad86c341960be71f3afcbba1b148a2bc
Granovsky Michael <osarthelighter@gmail.com>
2026-03-13 01:30:18 +0300

    added some more exercises to ExerciseRepository.cpp

--- src/data/ExerciseRepository.h
+++ src/data/ExerciseRepository.h
@@ -12,23 +12,14 @@ class ExerciseRepository
 {
 public:
     ExerciseRepository();
-
     QStringList exerciseNamesForMuscle(const QString& muscleId) const;
-
     void addExercise(const Exercise& e);
 
     QStringList allMuscleIds() const;
 
-    // optional
     std::optional<Exercise> findExerciseByName(const QString& name) const;
-
-    // exceptions
     Exercise getExerciseOrThrow(const QString& name) const;
-
-    // unique_ptr
     std::unique_ptr<Exercise> createExerciseCopy(const QString& name) const;
-
-    // shared_ptr
     std::shared_ptr<Exercise> getSharedExercise(const QString& name) const;
 
 private:
