bd94f01daa08919b7328bc5932ffd8c7366f7644
Granovsky Michael <osarthelighter@gmail.com>
2026-03-12 17:21:44 +0300

    changed struct Exercise to class Exercise

--- src/data/Exercise.h
+++ src/data/Exercise.h
@@ -2,20 +2,27 @@
 
 #include <QString>
 
-struct Exercise
+class Exercise
 {
+private:
     QString muscleId;
     QString name;
     QString difficulty;
     QString equipment;
 
-    Exercise(QString muscleId,
-             QString name,
-             QString difficulty,
-             QString equipment)
+public:
+    Exercise(const QString& muscleId,
+             const QString& name,
+             const QString& difficulty,
+             const QString& equipment)
         : muscleId(muscleId),
         name(name),
         difficulty(difficulty),
         equipment(equipment)
     {}
+
+    QString getMuscleId() const { return muscleId; }
+    QString getName() const { return name; }
+    QString getDifficulty() const { return difficulty; }
+    QString getEquipment() const { return equipment; }
 };
