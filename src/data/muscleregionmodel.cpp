763c5c5977cc0a542e956d19e8c8c310cce0c23b
BuzuevLev <buzuevlev@gmail.com>
2026-03-12 18:39:18 +0300

    Added class which creates clickable buttons on given coordinates

---
+++ src/data/muscleregionmodel.cpp
@@ -0,0 +1,3 @@
+#include "muscleregionmodel.h"
+
+muscleregionmodel::muscleregionmodel() {}
---
+++ src/data/muscleregionmodel.h
@@ -0,0 +1,10 @@
+#ifndef MUSCLEREGIONMODEL_H
+#define MUSCLEREGIONMODEL_H
+
+class muscleregionmodel
+{
+public:
+    muscleregionmodel();
+};
+
+#endif // MUSCLEREGIONMODEL_H
